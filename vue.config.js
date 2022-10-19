20a43ae9f82b238696b0bdb7ee2a90535d5b0239
uaykul <uaykul@gmail.com>
2022-10-19 14:32:11 +0300

    asd

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  //disableHostCheck: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   //disableHostCheck: true
+// })
+
+module.exports = {
+  devServer: {
+    disableHostCheck: true
+  }
+}
